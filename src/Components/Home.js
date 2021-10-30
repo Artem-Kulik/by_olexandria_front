23af4827d3b9087ccc248a8ca258b58de9c4b4d1
Artem-Kulik <art.kulik2005@gmail.com>
2021-10-30 20:15:50 +0300

    no-aim commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Home.css"
 
-export default function(){
-    return(
-        <div>
-            <h1>Hello world</h1>
-        </div>
+export default function () {
+    return (
+        <React.Fragment>
+            <div className="header">
+                <div className="header-part" />
+                <div className="header-part" />
+                <div className="header-part" />
+                <div className="header-part" />
+            </div>
+            <div className="body-fon">
+                <div className="try-block">
+                </div>
+                <div className="try-block">
+                </div>
+                <div className="try-block">
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
