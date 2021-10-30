aba9085493ac2ef42762dbe0fc0e0d362c28a94c
Artem-Kulik <art.kulik2005@gmail.com>
2021-10-30 20:34:09 +0300

    back-commit (

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,20 +4,6 @@ import "./Home.css"
 export default function () {
     return (
         <React.Fragment>
-            <div className="header">
-                <div className="header-part" />
-                <div className="header-part" />
-                <div className="header-part" />
-                <div className="header-part" />
-            </div>
-            <div className="body-fon">
-                <div className="try-block">
-                </div>
-                <div className="try-block">
-                </div>
-                <div className="try-block">
-                </div>
-            </div>
         </React.Fragment>
     )
 }
