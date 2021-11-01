8af042bf46095b372e154d307cb1b75df980bc6a
Artem-Kulik <art.kulik2005@gmail.com>
2021-11-01 11:20:07 +0200

    ВЫВОД

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,9 @@ import "./Home.css"
 
 export default function () {
     return (
-        <React.Fragment><h1> Ти чмошнік!!!</h1>
+        <React.Fragment>
+            <h1> Ти чмошнік!!!</h1>
+            <h1> Хіба ж це погано?</h1>
         </React.Fragment>
     )
 }
